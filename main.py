327db9a6871be2f08bb1817d445a33e7eabdc44d
UgoRastell <urastell@normandiewebschool.fr>
2023-01-12 16:39:20 +0100

    test

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import discord
-import json
+import asyncio
+from discord.ext import commands
+import interactions
 
-bot = discord.Bot()
+client = discord.Client()
 
-@bot.command(description="Sends the bot's latency.") 
-async def ping(ctx): 
-    await ctx.respond(f"Pong! Latency is {bot.latency}")
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print('------')
+
+@client.event
+async def on_message(message):
+    print(message.author.name)
 
 
-bot.run("MTA2MzA2NjAyMzY1OTU4MTQ1MA.GnQpWR.B7VcCepxr47dOgLeB6bpgWwNLqsbcsMRpvQBm0")
+@client.command(
+name="my_first_command",
+description="This is the first command I made!",
+)
+async def my_first_command(ctx: interactions.CommandContext):
+    await ctx.send("Hi there!")
+
+
+client.run("MTA2MzA2NjAyMzY1OTU4MTQ1MA.GEG0rX.Wb3n9e0X30umNxJyPkqIyWSPNknrhzJSkxQdfs")
 
